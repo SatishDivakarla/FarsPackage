0a8f0e6f34e30488db6bd7cbc860dcac2fb14200
Satish Divakarla <Satish.Divakarla@blackboard.com>
2017-04-02 11:19:22 -0700

    Corrected tests

--- tests/testthat/test-fars-functions.R
+++ tests/testthat/test-fars-functions.R
@@ -2,15 +2,16 @@ require(testthat)
 require("FarsPackage")
 
 test_that("fars_read reads valid file", {
-  valid_filename <- "accident_2013.csv.bz2"
+  setwd("../..")
+  valid_filename <- "data/accident_2013.csv.bz2"
   result_table <- fars_read(valid_filename)
   expect_that(result_table, is_a("data.frame"), label = "result_table should be an object of data.frame")
   expect_that(result_table, is_a("tbl"), label = "result_table should be an object of tbl")
   expect_that(result_table, is_a("tbl_df"), label = "result_table should be an object of tbl_df")
-  expect_that(length(result_table), equals(50), label = "length of result_table should be 50")
+  expect_equal(length(result_table), 50, label = "length of result_table should be 50")
 })
 
 test_that("fars_read fails when file does not exist", {
-  invalid_filename <- "accident_20143.csv.bz2"
+  invalid_filename <- "data/accident_20143.csv.bz2"
   expect_error(fars_read(invalid_filename), label= "Fars_read should fail when file does not exist")
 })
