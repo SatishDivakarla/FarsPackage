982b97747b5ccd33d3a7745cc27caf9875624054
Satish Divakarla <Satish.Divakarla@blackboard.com>
2017-04-02 16:00:13 -0700

    Corrected tests

--- tests/testthat/test-fars-functions.R
+++ tests/testthat/test-fars-functions.R
@@ -1,7 +1,10 @@
 require(testthat)
 require("FarsPackage")
 
-test_that("Make file name working",  {
+test_that("Make file name working with valid number",  {
   expect_equal(make_filename("2013"), "accident_2013.csv.bz2")
 })
 
+test_that("Make file name failed with non integer",  {
+  expect_warning(make_filename("test"))
+})
